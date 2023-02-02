a960e314ce64fa62f04555f0fa2f35a3d5b04a46
DotanOr <111221177+DotanOr@users.noreply.github.com>
2023-02-02 12:39:04 +0200

    Update load_libs.R

--- www/load_libs.R
+++ www/load_libs.R
@@ -2,6 +2,27 @@ message("Starting loading of libraries...")
 rm(list = ls(all = TRUE))
 lib_list <- c("shiny",
               "devtools",
+              "nlme",
+              "survival",
+              "cluster",
+              "foreign",
+              "nnet",
+              "reshape2",
+              "Rcpp",
+              "cowplot",
+              "tidyr",
+              "foreach",
+              "doParallel",
+              "rootSolve",
+              "deSolve",
+              "numDeriv",
+              "brglm2",
+              "PopED",
+              "pdftools",
+              "fpc",
+              "writexl",
+              "kableExtra",
+              "ResourceSelection",
               "IQRtools",
               "shinydashboard",
               "DT",
@@ -39,45 +60,7 @@ lib_list <- c("shiny",
               "ggsci",
               "promises",
               "future")
-lib_vers <- c('1.6.0',
-              '2.3.2',
-              '1.8.0',
-              '0.7.1',
-              '0.17',
-              '1.14.0',
-              '0.1.3',
-              '1.4.2',
-              '3.0.1',
-              '1.3.1',
-              '1.0.0',
-              '0.5.7',
-              '2.0.0',
-              '0.61',
-              '0.2.2',
-              '1.0.5',
-              '3.3.5',
-              '0.0.5',
-              '3.6.3',
-              '2.1.1',
-              '1.5.12.2',
-              '2.0.1',
-              '3.6.3',
-              '1.4.0',
-              '0.3.4',
-              '4.9.3',
-              '1.1.0',
-              '2.3',
-              '0.3.3',
-              '1.3.1',
-              '0.9.0',
-              '0.2.1',
-              '2.0.0',
-              '2.3.1',
-              '0.3.17',
-              '0.6.3',
-              '2.9',
-              '1.2.0.1',
-              '1.21.0')
+lib_vers <- c('1.6.0', '2.3.2', '3.1.144', '3.1.8', '2.1.0', '0.8.75', '7.3.12', '1.4.4', '1.0.8.3', '1.1.1', '1.1.3', '1.5.1', '1.0.16', '1.8.2.1', '1.28', '2016.8.1.1', '0.7.1', '0.6.0', '2.3.1', '2.2.9', '1.3.1', '1.3.4', '0.3.5', '1.8.0', '0.7.1', '0.17', '1.14.0', '0.1.3', '1.4.2', '3.0.1', '1.3.1', '1.0.0', '0.5.7', '2.0.0', '0.61', '0.2.2', '1.0.5', '3.3.5', '0.0.5', '3.6.3', '2.1.1', '1.5.12.2', '2.0.1', '3.6.3', '1.4.0', '0.3.4', '4.9.3', '1.1.0', '2.3', '0.3.3', '1.3.1', '0.9.0', '0.2.1', '2.0.0', '2.3.1', '0.3.17', '0.6.3', '2.9', '1.2.0.1', '1.21.0')
 names(lib_vers) <- lib_list
 is_installed <- function(mypkg) is.element(mypkg, installed.packages()[,1])
 load_or_install<-function(package_names)
@@ -90,7 +73,42 @@ load_or_install<-function(package_names)
       message(paste("installing packge",package_name,"Version",package_vers))
       if (package_name == "IQRtools"){
         source("https://iqrtoolsabc321.intiquan.com/install.R")
-        installVersion.IQRtools("1.8.0", forceDependencies = T)
+        local({
+          r <- getOption("repos")
+          r["CRAN"] <- repos
+          options(repos = r)
+        })
+        installVersion <- package_vers
+        # Download IQR Tools
+        url <- paste0("http://iqrtoolsabc321.intiquan.com/rrepo/src/contrib/IQRtools_",installVersion,".tar.gz")
+        destfile <- paste0("IQRtools_",installVersion,".tar.gz")
+        
+        doit <- TRUE
+        while (doit) {
+          cat()
+          doit <- tryCatch({
+            download.file(url,destfile,mode="wb",method=methodDownload)
+            FALSE
+          }, error = function(err) {
+            TRUE
+          })
+        }
+        
+        # Install IQR Tools
+        install.packages(destfile,repos=NULL,type="source",clean = TRUE)
+        unlink(destfile)
+        
+        # Install cOde and dMod if needed
+        x <- installed.packages()
+        path_cOdedMod <- paste0(x[rownames(x)=="IQRtools",colnames(x)=="LibPath"],"/IQRtools/dependencies")[1]
+        cOde <- list.files(path_cOdedMod,pattern = "cOde_*",full.names = TRUE)
+        dMod <- list.files(path_cOdedMod,pattern = "dMod_*",full.names = TRUE)
+        if (length(cOde) == 1) {
+          install.packages(cOde,repos=NULL,type="source")
+        }
+        if (length(dMod) == 1) {
+          install.packages(dMod,repos=NULL,type="source")
+        }
       }
       else if (package_name == "devtools") {
         install.packages("devtools")
