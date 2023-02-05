e86024e10b7d70252fe27e6671a2e592b00c7ac2
DotanOr <111221177+DotanOr@users.noreply.github.com>
2023-02-05 21:53:16 +0200

    Update gen_spag_obj.R

--- gen_spag_obj.R
+++ gen_spag_obj.R
@@ -1,22 +1,22 @@
 gen_spag_obj <- function(x,stratify) {
   #function takes data and generates a Spaghetti ggplot object (or list) without printing it 
-  require(IQRtools)
-  cur_dev <- grDevices::dev.cur()   # store current device
-  pdf(NULL, width = 6, height = 6)  # open null device
-  grDevices::dev.control("enable")  # turn on recording for the null device
-  null_dev <- grDevices::dev.cur()  # store null device
-  
-  # make sure we always clean up properly, even if something causes an error
-  on.exit({
-    grDevices::dev.off(null_dev)
-    if (cur_dev > 1) grDevices::dev.set(cur_dev) # only set cur device if not null device
-  })
-  
+  # require(IQRtools)
+  # cur_dev <- grDevices::dev.cur()   # store current device
+  # pdf(NULL, width = 6, height = 6)  # open null device
+  # grDevices::dev.control("enable")  # turn on recording for the null device
+  # null_dev <- grDevices::dev.cur()  # store null device
+  # 
+  # # make sure we always clean up properly, even if something causes an error
+  # on.exit({
+  #   grDevices::dev.off(null_dev)
+  #   if (cur_dev > 1) grDevices::dev.set(cur_dev) # only set cur device if not null device
+  # })
+  # 
   # plot
-  pdf(tempfile())
+  # pdf(tempfile())
   p<-plotSpaghetti_IQRdataGENERAL(data = x,
                                   stratify= stratify,
                                   FLAGreturnObject = TRUE)
-  dev.off()
+  # dev.off()
   return(unlist(p, recursive = FALSE))
 }
