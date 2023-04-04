48aab9bbb23ff6c7ac2ba5700773d09c0f20b270
DotanOr <111221177+DotanOr@users.noreply.github.com>
2023-04-04 19:35:42 +0300

    Update gen_spag_obj.R

--- gen_spag_obj.R
+++ gen_spag_obj.R
@@ -1,8 +1,8 @@
 gen_spag_obj <- function(x,stratify) {
-  #function takes data and generates a Spaghetti ggplot object (or list) without printing it 
-  p<-plotSpaghetti_IQRdataGENERAL(data = x,
-                                  stratify= stratify,
-                                  FLAGreturnObject = TRUE)
+  # function takes data and generates a Spaghetti ggplot object (or list) without printing it 
+  p <- plotSpaghetti_IQRdataGENERAL(data = x,
+                                    stratify = stratify,
+                                    FLAGreturnObject = TRUE)
   # dev.off()
   return(unlist(p, recursive = FALSE))
 }
